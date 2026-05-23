8b286b84bd9d648a185faeec1fb3093fb172d12e
jorj1357 <hello@mimita.fun>
2026-05-23 11:49:41 -0400

    better newsletter

--- website/src/pages/Newsletter.jsx
+++ website/src/pages/Newsletter.jsx
@@ -2,8 +2,12 @@ import "../App.css"
 
 import Layout from "../components/Layout"
 
+import NewsletterBox from "../components/NewsletterBox"
+
 export default function Newsletter() {
+
   return (
+
     <Layout>
 
       <div className="newsletterPage">
@@ -20,27 +24,7 @@ export default function Newsletter() {
           and the future of MiMITA
         </p>
 
-        <div className="newsletterForm">
-
-          <input
-            type="email"
-            placeholder="enter email"
-            className="newsletterInput"
-          />
-
-          <button className="newsletterButton">
-            SIGN UP
-          </button>
-
-        </div>
-
-        {/* <p className="newsletterSmall">
-          placeholder
-          <br></br>
-          future emails sent from:
-          <br></br>
-          hello@mimita.fun
-        </p> */}
+        <NewsletterBox />
 
       </div>
 
