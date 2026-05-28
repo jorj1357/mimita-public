006b750cab2b97ddafdbd401dbd5de7f56674afe
jorj1357 <hello@mimita.fun>
2026-05-28 12:36:20 -0400

    bunch of stuff like icons etc

--- website/src/pages/Socials-jorj.jsx
+++ website/src/pages/Socials-jorj.jsx
@@ -1,3 +1,4 @@
+
 import "../App.css"
 
 import Layout from "../components/Layout"
@@ -8,11 +9,9 @@ export default function Jorj() {
 
       <div className="socialsPage">
 
-         <h1 className="socialsTitle">
+        <h1 className="socialsTitle">
           jorj1357 socials
         </h1>
-
-        {/* SOCIAL LINKS */}
 
         <div className="socialsLinks">
 
@@ -22,7 +21,15 @@ export default function Jorj() {
             rel="noopener noreferrer"
             className="socialLink"
           >
-            jorj1357 : youtube
+            <img
+              src="/youtube icon v1-optimized.webp"
+              alt="youtube"
+              className="socialIcon"
+            />
+
+            <span>
+              jorj1357 : youtube
+            </span>
           </a>
 
           <a
@@ -31,16 +38,15 @@ export default function Jorj() {
             rel="noopener noreferrer"
             className="socialLink"
           >
-            jorj1357 : spotify
-          </a>
+            <img
+              src="/spotify logo v1-optimized.webp"
+              alt="spotify"
+              className="socialIcon"
+            />
 
-           <a
-            href="https://soundcloud.com/user-434584060"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="socialLink"
-          >
-            jorj1357 : soundcloud
+            <span>
+              jorj1357 : spotify
+            </span>
           </a>
 
           <a
@@ -49,7 +55,15 @@ export default function Jorj() {
             rel="noopener noreferrer"
             className="socialLink"
           >
-            jorj1357 : discord
+            <img
+              src="/discord icon v1-optimized.webp"
+              alt="discord"
+              className="socialIcon"
+            />
+
+            <span>
+              jorj1357 : discord
+            </span>
           </a>
 
           <a
@@ -58,7 +72,15 @@ export default function Jorj() {
             rel="noopener noreferrer"
             className="socialLink"
           >
-            jorj1357 : tiktok
+            <img
+              src="/tiktok icon v1-optimized.webp"
+              alt="tiktok"
+              className="socialIcon"
+            />
+
+            <span>
+              jorj1357 : tiktok
+            </span>
           </a>
 
           <a
@@ -67,87 +89,26 @@ export default function Jorj() {
             rel="noopener noreferrer"
             className="socialLink"
           >
-            jorj1357 : instagram
-               <br></br>
-        
-        <br></br>
-        
-        <br></br>
+            <img
+              src="/instagram icon v1-optimized.webp"
+              alt="instagram"
+              className="socialIcon"
+            />
+
+            <span>
+              jorj1357 : instagram
+            </span>
           </a>
-
-          <a
-            href="https://x.com/jorj1357/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="socialLink"
-          >
-            jorj1357 : X/xitter/twitter/X, The Everything App
-            
-     
-          </a>
-          
-
-           <a
-            href="https://jorj1357.bandcamp.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="socialLink"
-          >
-            jorj1357 : bandcamp
-          </a>
-
-           <a
-            href="https://www.patreon.com/c/jorj1357v2"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="socialLink"
-          >
-            jorj1357 : patreon (kinda  sucks)
-          </a>
-
-           <a
-            href="https://www.patreon.com/c/jorj1357v2"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="socialLink"
-          >
-            jorj1357 : ko-fi (kinda  sucks double)
-          </a>
-
-          <a
-            href="https://osu.ppy.sh/users/15943191"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="socialLink"
-          >
-            jorj1357 : osu! (UGHHHHHHHhhhhhhhhhh)
-          </a>
-
-            {/* https://www.patreon.com/c/jorj1357v2 */}
-            {/* https://x.com/jorj1357 */}
-
-           <a
-            href="https://linktr.ee/jorj1357"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="socialLink"
-          >
-            jorj1357 : linktree (thx linktree)
-          </a>
-
-          {/* https://jorj1357.bandcamp.com/ */}
 
         </div>
 
-       
-
-         <h1 className="socialsTitle">
-             <br></br>
-
-        <br></br>
-        <br></br>
-        <br></br>
-          jorj1357: about
+        <h1
+          className="socialsTitle"
+          style={{
+            marginTop: "100px"
+          }}
+        >
+          jorj1357 : about
         </h1>
 
         <p
@@ -155,67 +116,47 @@ export default function Jorj() {
           style={{
             maxWidth: "700px",
             margin: "0 auto 40px auto",
-            textAlign: "center"
+            textAlign: "center",
+            lineHeight: "2"
           }}
         >
-          this is how i initially started on the internet 
-          <br></br>
-          <br></br>
-           <br></br>
-          <br></br>
-          i still remember sitting in 10th grade history 
-           <br></br>
-          <br></br>
-          sneak watching this channel on my phone 
-           <br></br>
-          <br></br>
-          go from like 100 subs to 900 
-           <br></br>
-          <br></br>
+          this is how i initially started on the internet
+
+          <br /><br />
+
+          i still remember sitting in 10th grade history
+
+          <br /><br />
+
+          sneak watching this channel on my phone
+
+          <br /><br />
+
+          go from like 100 subs to 900
+
+          <br /><br />
+
           in a week
-           <br></br>
-          <br></br>
-          
-           <br></br>
-          <br></br>
+
+          <br /><br />
 
           this project taught me & continues to teach me
-           <br></br>
-          <br></br> 
-          {/* how ridonkulous the internet can be  */}
-          how counter intuitive the internet can be 
-           <br></br>
-          <br></br>
-          and how something so insignificant to u 
-           <br></br>
-          <br></br>
-          can become someone's entire world
-          {/* im serlf conscous about this part so i didnt include it  */}
-          {/* sometimes even thousands, or tens , hundreds, millions, grillions, of thousands
-          a random 12 second short that u think will get 3 views and 1 dislike
-           <br></br>
-          <br></br>
-          can become like 5 million like view shares overnight 
-           <br></br>
-          <br></br>
-          #insprirtrtiational
-          <br></br>
-          <br></br> */}
 
-          {/* i dont know where this project will go 
-           <br></br>
-          <br></br>
-          or what its end goal is
-          <br></br>
-          <br></br>
-          other than getting to assorted sounds 99 (5/25/2026 current is 19.5)
-          <br></br>
-          <br></br>
-          but i do know this is why other projects like rav growth and 3cage and mimita exist */}
-          <br></br>
-          <br></br>
-          --- --- --- --- --- --- --- --- --- 
-          --- --- --- --- --- --- --- --- --- 
+          <br /><br />
+
+          how counter intuitive the internet can be
+
+          <br /><br />
+
+          and how something so insignificant to u
+
+          <br /><br />
+
+          can become someone's entire world
+
+          <br /><br />
+
+          --- --- --- --- --- --- --- --- ---
         </p>
 
       </div>
