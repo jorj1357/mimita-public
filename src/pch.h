8cfbb6bed44887a6f985719bd31900d88f0ea40b
jorj1357 <hello@mimita.fun>
2026-06-06 15:26:55 -0400

    WOKRING BUILD BETTER I TINK

---
+++ src/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// STL
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include <algorithm>
+#include <functional>
+#include <optional>
+
+// Windows
+#include <windows.h>
+
+// OpenGL
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
