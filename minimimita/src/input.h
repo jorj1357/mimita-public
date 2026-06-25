4c68c5e34b76292a0c351f20a558dae078f231f7
jorj1357 <hello@mimita.fun>
2026-06-24 20:51:35 -0400

    just  mkae  on the main repo

--- minimimita/src/input.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <GLFW/glfw3.h>
-
-void keyCallback(GLFWwindow* win, int key, int scancode, int action, int mods);
-void cursorPosCallback(GLFWwindow* win, double x, double y);
-void scrollCallback(GLFWwindow* win, double xoffset, double yoffset);
-void windowSizeCallback(GLFWwindow* win, int w, int h);
-void errorCallback(int err, const char* desc);
--- minimimita/src/map-utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void loadTestMap(int idx);
-void loadGLBMap(const char* name);
-void executeCommand(const char* cmd);
-void processCommand();
