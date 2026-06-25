63f37d1b766dcb5c9ca09219150cf8d968022a14
jorj1357 <hello@mimita.fun>
2026-06-24 20:51:35 -0400

    just  mkae  on the main repo

--- minimimita/src/physics.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "types.h"
-
-bool capsuleTriangleCollision(glm::vec3 capA, glm::vec3 capB, float radius,
-                              const Triangle& tri, Contact& contact);
-void computeWishDir(InputState& input, const Camera& camera);
-void updatePlayer(Player& player, const InputState& input,
-                  const std::vector<Triangle>& triangles, float dt);
